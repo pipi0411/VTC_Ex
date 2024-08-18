2bd4d1efa1c61e8066424cf3cdc32df437186579
pipi0411 <20a41010217@students.hou.edu.vn>
2024-08-18 20:09:58 +0700

    new lab7_c

---
+++ Lab7_161_08_2024/Lab7_2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main () {
+
+int arr[100];
+
+srand(time(NULL));
+    for (int i = 0; i < 100; i++)
+    {
+        arr[i] = rand()%1000;
+    }
+
+     for (int i = 0; i < 100; i++)
+    {
+        for (int j = 0; j < 100 - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+ printf("Display array:\n");
+    for (int i = 0; i < 100; i++)
+    {
+
+        printf("%4d",arr[i]);
+        if((i+1)%10==0) {
+            printf("\n");
+        }
+
+    }
+    return 0;
+}
