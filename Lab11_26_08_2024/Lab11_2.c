940c3f6c51602da8e11f5398dfb4f2e75c725fb9
pipi0411 <142613653+pipi0411@users.noreply.github.com>
2024-08-28 18:27:27 +0700

    Update Lab11_2.c

--- Lab11_26_08_2024/Lab11_2.c
+++ Lab11_26_08_2024/Lab11_2.c
@@ -1,29 +1,49 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct product{
-    char name[80];
-    char amount[5];
-    float price;
+#define MAX_PRODUCTS 5
+#define MAX_NAME_LENGTH 50
+
+
+typedef struct {
+    char name[MAX_NAME_LENGTH];
+    int quantity;
+    int price;
+    int total;
 } Product;
 
+int main() {
+    Product products[MAX_PRODUCTS];
+    int grandTotal = 0;
 
-int main(){
-    for(int i = 0; i < 5; i++){
-        Product p;
-        printf("Product name: ");
-        fgets(p.name, 80, stdin);
-        p.name[strcspn(p.name, "\n")] = '\0';
-        printf("Amount: ");
-        fgets(p.amount, 5, stdin);
-        p.amount[strcspn(p.amount, "\n")] = '\0';
-        printf("Price: ");
-        scanf("%f", &p.price);
-        char ch;
-        while((ch = getchar()) != '\n' && ch != EOF);
-   
+    for (int i = 0; i < MAX_PRODUCTS; i++) {
+        printf("Nhap thong tin cho san pham thu %d:\n", i + 1);
+
+        printf("Ten san pham: ");
+        fgets(products[i].name, MAX_NAME_LENGTH, stdin);
+        products[i].name[strcspn(products[i].name, "\n")] = 0;
+
+        printf("So luong: ");
+        scanf("%d", &products[i].quantity);
+
+        printf("Don gia: ");
+        scanf("%d", &products[i].price);
+
+        products[i].total = products[i].quantity * products[i].price;
+        grandTotal += products[i].total;
+
+
+        while (getchar() != '\n');
     }
-    printf("DANH MUC SAN PHAM\n");
+
+    printf("\nDANH MUC SAN PHAM\n");
+    printf("%-5s%-20s%-10s%-10s%-10s\n", "STT", "Ten san pham", "So luong", "Don gia", "Tong tien");
+
+    for (int i = 0; i < MAX_PRODUCTS; i++) {
+        printf("%-5d%-20s%-10d%-10d%-10d\n", i + 1, products[i].name, products[i].quantity, products[i].price, products[i].total);
+    }
+
+    printf("Tong cong: %d\n", grandTotal);
 
     return 0;
 }
