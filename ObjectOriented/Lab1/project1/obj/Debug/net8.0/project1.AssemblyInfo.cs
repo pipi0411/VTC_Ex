f677f2e8bb29d0888d74f3e407ee136b7c59eb3a
pipi0411 <20a41010217@students.hou.edu.vn>
2024-11-17 09:30:03 +0700

    update

--- ObjectOriented/Lab1/project1/obj/Debug/net8.0/project1.AssemblyInfo.cs
+++ ObjectOriented/Lab1/project1/obj/Debug/net8.0/project1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2feba01bafe07b39e2c5ab1cfd3340edc64e5161")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f8fec6dce54b4e0ec71abdb1dce1dd7116daa51")]
 [assembly: System.Reflection.AssemblyProductAttribute("project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
